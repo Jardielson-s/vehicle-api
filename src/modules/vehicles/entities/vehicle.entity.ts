841a24a3856fe95c7bbd44f9f6fdd4a5709f15c5
Jardielson <jardielsonbio@gmail.com>
2025-09-13 13:26:35 -0300

    feat: create base struct

---
+++ src/modules/vehicles/entities/vehicle.entity.ts
@@ -0,0 +1,33 @@
+import { Types } from 'mongoose';
+
+export class VehicleEntity {
+  _id: string;
+  placa: string;
+  chassi: string;
+  renavam: string;
+  modelo: string;
+  marca: string;
+  ano: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+
+  constructor(input: VehicleEntity) {
+    Object.assign(this, input);
+  }
+
+  static create(
+    input: Pick<
+      VehicleEntity,
+      'ano' | 'chassi' | 'marca' | 'modelo' | 'placa' | 'renavam'
+    >,
+  ): VehicleEntity {
+    return new VehicleEntity({
+      ...input,
+      _id: new Types.ObjectId().toString(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+    });
+  }
+}
